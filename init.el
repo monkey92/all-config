c91f38c435852ce9000a7cbda40d10410617324f
monkey92 <2223445830@qq.com>
2018-10-25 19:54:38 +0800

    Create init.el

---
+++ init.el
@@ -0,0 +1,16 @@
+;; my config
+;;(display-time-mode t)
+;;关闭工具条
+(tool-bar-mode 0)
+;;关闭启动动画
+(setq inhibit-startup-message t)
+;;高亮当前行
+(global-hl-line-mode 1)
+;;不生成备份文件
+(setq make-backup-files nil)
+;;设置行号
+;;(setq line-number-mode t)
+;;(global-linum-mode t)
+;;设置光标
+(setq-default cursor-type 'bar )
+;;
